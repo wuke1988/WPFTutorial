1bdcac686aa82af2c1cbad265ff1588242f8656f
wuke1988 <470041973@qq.com>
2016-11-06 16:45:44 +0800

    完成DependencyProperty 和 RoutedEvent Tutorial

--- DependencyPropertyAndRoutedEvent/Circle.cs
+++ DependencyPropertyAndRoutedEvent/Circle.cs
@@ -24,18 +24,17 @@ namespace DependencyPropertyAndRoutedEvent
             get { return (Brush)GetValue(FillProperty); }
             set { SetValue(FillProperty, value); }
         }
-        public static readonly DependencyProperty FillProperty = DependencyProperty.Register("FillProperty", typeof(Brush), typeof(Circle),
+        public static readonly DependencyProperty FillProperty = DependencyProperty.Register("Fill", typeof(Brush), typeof(Circle),
             new FrameworkPropertyMetadata(Brushes.Red, FrameworkPropertyMetadataOptions.AffectsRender, OnFillPropertyChanged));
 
         private static void OnFillPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             ((UIElement)d).RaiseEvent(new FillChangedRoutedEventArgs((Brush)e.NewValue)
             {
-                Source = d,
-                RoutedEvent = FillChangedEvent
+                RoutedEvent = FillChangedEvent,
+                Source = d
             });
         }
-
 
 
         public event EventHandler<FillChangedRoutedEventArgs> FillChanged
@@ -52,22 +51,23 @@ namespace DependencyPropertyAndRoutedEvent
             set { SetValue(ThicknessProperty, value); }
             get { return (double)GetValue(ThicknessProperty); }
         }
-        public static readonly DependencyProperty ThicknessProperty = DependencyProperty.Register("ThicknessProperty",typeof(Circle),
-            new FrameworkPropertyMetadata(1.0,FrameworkPropertyMetadataOptions.AffectsRender,null, coerceValueCallBack),validateValueCallback);
+        public static readonly DependencyProperty ThicknessProperty = DependencyProperty.Register("Thickness",typeof(double),typeof(Circle),
+            new FrameworkPropertyMetadata(1.0,FrameworkPropertyMetadataOptions.AffectsRender,null, coerceValueCallBack), OnValidateThicknessProperty);
 
-        public static object coerceValueCallBack(DependencyObject d, object baseValue)
+        private static object coerceValueCallBack(DependencyObject d, object baseValue)
         {
-            int value = (int)baseValue;
+            var value = (double)baseValue;
             if (value > 0)
                 return Math.Min(value, 2);
             return baseValue;
         }
-        public static object validateValueCallback(DependencyObject d, object baseValue)
+        private static bool OnValidateThicknessProperty(object value)
         {
-
+            return (double)value >= 0;
         }
 
-    protected override void OnRender(DrawingContext drawingContext)
+
+        protected override void OnRender(DrawingContext drawingContext)
         {
             var radius = Math.Min(ActualWidth, ActualHeight) / 2;
             drawingContext.DrawEllipse(Fill,new Pen(), new Point(ActualWidth / 2, ActualHeight / 2),radius,radius);
--- DependencyPropertyAndRoutedEvent/MainWindow.xaml.cs
+++ DependencyPropertyAndRoutedEvent/MainWindow.xaml.cs
@@ -27,13 +27,29 @@ namespace DependencyPropertyAndRoutedEvent
 
         private void circle_FillChanged(object sender, FillChangedRoutedEventArgs e)
         {
-
+            MessageBox.Show($@"Fill is Changed to {e.Fill}");
         }
 
         private void circle_MouseDown(object sender, MouseButtonEventArgs e)
         {
             MessageBox.Show($@" {sender.ToString()}  Mouse is down!");
             //e.Handled = true;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.circle.Thickness = 10;
+            circle.Fill = Brushes.Blue;
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("NEW");
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
         }
     }
 }
