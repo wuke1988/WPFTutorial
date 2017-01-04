d69eda08178594d9fbec056ac8f1faa4f3b9f643
吴可 <wuke6666@163.com>
2017-01-04 21:21:10 +0800

    家里的代码提交

---
+++ Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public string Gender { get; set; }
+
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Name: {0}", Name);
+        }
+
+        public static Person John = new Person { Name = "John" };
+    }
+}
