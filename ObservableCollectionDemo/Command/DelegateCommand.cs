999f47b79dd01436b9407f4efdbbc548b5f0fb72
吴可 <wuke6666@163.com>
2017-08-12 17:36:58 +0800

    添加实现基于MVVM的WPF简单示例代码

---
+++ ObservableCollectionDemo/Command/DelegateCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ObservableCollectionDemo.Command
+{
+    public class DelegateCommand : ICommand
+    {
+        readonly Action<object> _execute;
+        readonly Predicate<object> _canExcute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public DelegateCommand(Action<object> action)
+            :this(action,null)
+        {
+
+        }
+        public DelegateCommand(Action<object> action, Predicate<object> predicate)
+        {
+            _execute = action;
+            _canExcute = predicate;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExcute != null ? _canExcute(parameter):true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute?.Invoke(parameter);
+        }
+    }
+}
