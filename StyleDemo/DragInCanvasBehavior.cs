a55d9dae4be75be10f899eaac6010498d27add28
吴可 <470041973@qq.com>
2017-10-16 16:35:49 +0800

    实践 样式 和行为部分

---
+++ StyleDemo/DragInCanvasBehavior.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+using System.Windows.Media;
+
+namespace StyleDemo
+{
+    public class DragInCanvasBehavior:Behavior<UIElement>
+    {
+        
+        private Canvas canvas;
+        private bool isDragging = false;
+        /// <summary>
+        /// 当元素被点击后，记录准确的点击位置
+        /// </summary>
+        private Point mouseOffset;
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            this.AssociatedObject.MouseLeftButtonDown += AssociatedObject_MouseLeftButtonDown;
+
+            this.AssociatedObject.MouseMove += AssociatedObject_MouseMove;
+
+            this.AssociatedObject.MouseLeftButtonUp += AssociatedObject_MouseLeftButtonUp;
+
+        }
+       
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AssociatedObject_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (canvas == null)
+                canvas = (Canvas)VisualTreeHelper.GetParent(this.AssociatedObject);
+
+            isDragging = true;
+
+            mouseOffset = e.GetPosition(AssociatedObject);
+
+            AssociatedObject.CaptureMouse();
+
+            
+        }
+
+        private void AssociatedObject_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                Point point = e.GetPosition(canvas);
+
+                AssociatedObject.SetValue(Canvas.TopProperty, point.Y - mouseOffset.Y);
+                AssociatedObject.SetValue(Canvas.LeftProperty, point.X - mouseOffset.X);
+
+            }
+
+        }
+
+        private void AssociatedObject_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (isDragging)
+            {
+                AssociatedObject.ReleaseMouseCapture();
+                isDragging = false;
+            }
+        }
+
+    }
+}
