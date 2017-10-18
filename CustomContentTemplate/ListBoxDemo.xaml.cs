f72826440e91d9cc38f685d3a680a69d9e06dd2c
吴可 <470041973@qq.com>
2017-10-18 16:56:53 +0800

    完成控件模板实例

---
+++ CustomContentTemplate/ListBoxDemo.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CustomContentTemplate
+{
+    /// <summary>
+    /// ListBoxDemo.xaml 的交互逻辑
+    /// </summary>
+    public partial class ListBoxDemo : Window
+    {
+        public ListBoxDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            listBox.ItemsSource = new List<String> {"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten" };
+        }
+    }
+}
