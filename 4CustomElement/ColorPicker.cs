5e976d5909baee72e83de81cc9e51fc4f41c9f80
吴可 <470041973@qq.com>
2017-10-19 18:35:52 +0800

    创建无外观控件demo

--- 4CustomElement/ColorPicker.cs
+++ 4CustomElement/ColorPicker.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -16,9 +17,39 @@ using System.Windows.Shapes;
 namespace _4CustomElement
 {
     /// <summary>
-    /// UserControl.xaml 的交互逻辑
+    /// 按照步骤 1a 或 1b 操作，然后执行步骤 2 以在 XAML 文件中使用此自定义控件。
+    ///
+    /// 步骤 1a) 在当前项目中存在的 XAML 文件中使用该自定义控件。
+    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根 
+    /// 元素中: 
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:_4CustomElement"
+    ///
+    ///
+    /// 步骤 1b) 在其他项目中存在的 XAML 文件中使用该自定义控件。
+    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根 
+    /// 元素中: 
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:_4CustomElement;assembly=_4CustomElement"
+    ///
+    /// 您还需要添加一个从 XAML 文件所在的项目到此项目的项目引用，
+    /// 并重新生成以避免编译错误: 
+    ///
+    ///     在解决方案资源管理器中右击目标项目，然后依次单击
+    ///     “添加引用”->“项目”->[浏览查找并选择此项目]
+    ///
+    ///
+    /// 步骤 2)
+    /// 继续操作并在 XAML 文件中使用控件。
+    ///
+    ///     <MyNamespace:ColorPicker/>
+    ///
     /// </summary>
-    public partial class ColorPickerUserControl : UserControl
+    [TemplatePart(Name = "PART_RedSlider", Type =typeof(RangeBase))]
+    [TemplatePart(Name = "PART_BlueSlider", Type = typeof(RangeBase))]
+    [TemplatePart(Name = "PART_GreenSlider", Type = typeof(RangeBase))]
+    [TemplatePart(Name = "PART_PreviewBrush", Type = typeof(FrameworkElement))]
+    public class ColorPicker : Control
     {
         public static DependencyProperty ColorProperty;
 
@@ -62,47 +93,28 @@ namespace _4CustomElement
 
         private static Color? previousColor;
 
-        static ColorPickerUserControl()
+        static ColorPicker()
         {
-            ColorProperty = DependencyProperty.Register("Color", typeof(Color), typeof(ColorPickerUserControl),
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ColorPicker), new FrameworkPropertyMetadata(typeof(ColorPicker)));
+
+            ColorProperty = DependencyProperty.Register("Color", typeof(Color), typeof(ColorPicker),
                 new PropertyMetadata(Colors.Black, new PropertyChangedCallback(OnColorChanged)));
 
-            RedProperty = DependencyProperty.Register("Red", typeof(byte), typeof(ColorPickerUserControl),
+            RedProperty = DependencyProperty.Register("Red", typeof(byte), typeof(ColorPicker),
                 new PropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
 
-            BlueProperty = DependencyProperty.Register("Blue", typeof(byte), typeof(ColorPickerUserControl),
+            BlueProperty = DependencyProperty.Register("Blue", typeof(byte), typeof(ColorPicker),
                 new PropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
 
-            GreenProperty = DependencyProperty.Register("Green", typeof(byte), typeof(ColorPickerUserControl),
+            GreenProperty = DependencyProperty.Register("Green", typeof(byte), typeof(ColorPicker),
                 new PropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
 
             ColorChangedEvent = EventManager.RegisterRoutedEvent("ColorChanged", RoutingStrategy.Bubble,
-                typeof(RoutedPropertyChangedEventHandler<Color>), typeof(ColorPickerUserControl));
-        }
-
-        public ColorPickerUserControl()
-        {
-            InitializeComponent();
-            SetUpCommands();
-        }
-
-        private void SetUpCommands()
-        {
-            //设置命令绑定
-            CommandBinding commandBinding = new CommandBinding(ApplicationCommands.Undo,UndoCommand_Excuted, UndoCommand_CanExcuted);
-            this.CommandBindings.Add(commandBinding);
-        }
-        private void UndoCommand_Excuted(object sender,ExecutedRoutedEventArgs e)
-        {
-            Color = (Color)previousColor;
-        }
-        private void UndoCommand_CanExcuted(object sender, CanExecuteRoutedEventArgs e)
-        {
-            e.CanExecute = true;
+                typeof(RoutedPropertyChangedEventHandler<Color>), typeof(ColorPicker));
         }
         private static void OnColorChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
         {
-            ColorPickerUserControl colorPicker = (ColorPickerUserControl)sender;
+            ColorPicker colorPicker = (ColorPicker)sender;
             colorPicker.Color = (Color)e.NewValue;
 
             colorPicker.Red = ((Color)e.NewValue).R;
@@ -113,14 +125,14 @@ namespace _4CustomElement
                 new RoutedPropertyChangedEventArgs<Color>((Color)e.OldValue, colorPicker.Color);
             arg.RoutedEvent = ColorChangedEvent;
 
-            previousColor = (Color?)e.OldValue??null;
+            previousColor = (Color?)e.OldValue ?? null;
 
             colorPicker.RaiseEvent(arg);
         }
 
         private static void OnColorRGBChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
         {
-            ColorPickerUserControl colorPicker = (ColorPickerUserControl)sender;
+            ColorPicker colorPicker = (ColorPicker)sender;
             Color color = colorPicker.Color;
 
             if (e.Property == RedProperty)
@@ -132,6 +144,24 @@ namespace _4CustomElement
 
             colorPicker.Color = color;
         }
+        /// <summary>
+        /// 在此方法内可以在模板内查找元素(使用GetTemplateChild())并关联事件或添加数据绑定表达式
+        /// </summary>
+        //public override void OnApplyTemplate()
+        //{
+        //    base.OnApplyTemplate();
+
+        //    //手工设置控件绑定
+        //    RangeBase slider = GetTemplateChild("PART_RedSlider") as RangeBase;
+        //    if (slider != null)
+        //    {
+        //        Binding binding = new Binding("Red");
+        //        binding.Source = this;
+        //        binding.Mode = BindingMode.TwoWay;
+
+        //        slider.SetBinding(RangeBase.ValueProperty,binding);
+        //    }
+        //}
 
     }
 }
