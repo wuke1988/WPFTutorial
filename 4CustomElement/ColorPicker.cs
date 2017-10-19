3d25b5b9494e67403ff1c40c71b4d5b8fabd3bf8
吴可 <470041973@qq.com>
2017-10-19 14:21:28 +0800

    添加用户元素 实例

---
+++ 4CustomElement/ColorPicker.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _4CustomElement
+{
+    /// <summary>
+    /// 按照步骤 1a 或 1b 操作，然后执行步骤 2 以在 XAML 文件中使用此自定义控件。
+    ///
+    /// 步骤 1a) 在当前项目中存在的 XAML 文件中使用该自定义控件。
+    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根 
+    /// 元素中: 
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:_4CustomElement"
+    ///
+    ///
+    /// 步骤 1b) 在其他项目中存在的 XAML 文件中使用该自定义控件。
+    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根 
+    /// 元素中: 
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:_4CustomElement;assembly=_4CustomElement"
+    ///
+    /// 您还需要添加一个从 XAML 文件所在的项目到此项目的项目引用，
+    /// 并重新生成以避免编译错误: 
+    ///
+    ///     在解决方案资源管理器中右击目标项目，然后依次单击
+    ///     “添加引用”->“项目”->[选择此项目]
+    ///
+    ///
+    /// 步骤 2)
+    /// 继续操作并在 XAML 文件中使用控件。
+    ///
+    ///     <MyNamespace:CustomControl1/>
+    ///
+    /// </summary>
+    public class ColorPicker : UserControl
+    {
+        public static DependencyProperty ColorProperty;
+
+        public Color Color
+        {
+            set { SetValue(ColorProperty,value); }
+            get { return (Color)GetValue(ColorProperty); }
+        }
+
+        public static DependencyProperty RedProperty;
+
+        public byte Red
+        {
+            set { SetValue(RedProperty, value); }
+            get { return (byte)GetValue(RedProperty); }
+        }
+
+        public static DependencyProperty BlueProperty;
+
+        public byte Blue
+        {
+            set { SetValue(BlueProperty, value); }
+            get { return (byte)GetValue(BlueProperty); }
+        }
+
+        public static DependencyProperty GreenProperty;
+
+        public byte Green
+        {
+            set { SetValue(GreenProperty, value); }
+            get { return (byte)GetValue(GreenProperty); }
+        }
+
+        public static readonly RoutedEvent ColorChangedEvent;
+
+        public event RoutedPropertyChangedEventHandler<Color> ColorChanged
+        {
+            add { AddHandler(ColorChangedEvent, value); }
+            remove { RemoveHandler(ColorChangedEvent,value); }
+        }
+
+        static ColorPicker()
+        {
+            ColorProperty = DependencyProperty.Register("Color", typeof(Color), typeof(ColorPicker),
+                new PropertyMetadata(Colors.Black, new PropertyChangedCallback(OnColorChanged)));
+
+            RedProperty = DependencyProperty.Register("Red", typeof(byte), typeof(ColorPicker),
+                new PropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
+
+            BlueProperty = DependencyProperty.Register("Blue", typeof(byte), typeof(ColorPicker),
+                new PropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
+
+            GreenProperty = DependencyProperty.Register("Green", typeof(byte), typeof(ColorPicker),
+                new PropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
+
+            ColorChangedEvent = EventManager.RegisterRoutedEvent("ColorChanged",RoutingStrategy.Bubble,
+                typeof(RoutedPropertyChangedEventHandler<Color>),typeof(ColorPicker));
+        }  
+
+        private static void OnColorChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            ColorPicker colorPicker = (ColorPicker)sender;
+            colorPicker.Color = (Color)e.NewValue;
+
+            colorPicker.Red = ((Color)e.NewValue).R;
+            colorPicker.Green = ((Color)e.NewValue).G;
+            colorPicker.Blue = ((Color)e.NewValue).B;
+
+            RoutedPropertyChangedEventArgs<Color> arg =
+                new RoutedPropertyChangedEventArgs<Color>((Color)e.OldValue,colorPicker.Color);
+            arg.RoutedEvent = ColorChangedEvent;
+
+            colorPicker.RaiseEvent(arg);
+        }
+
+        private static void OnColorRGBChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            ColorPicker colorPicker = (ColorPicker)sender;
+            Color color = colorPicker.Color;
+
+            if (e.Property == RedProperty)
+                color.R = (byte)e.NewValue;
+            if (e.Property == BlueProperty)
+                color.B = (byte)e.NewValue;
+            if (e.Property == GreenProperty)
+                color.G = (byte)e.NewValue;
+
+            colorPicker.Color = color;
+        }
+    }
+}
