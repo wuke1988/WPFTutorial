50ad70afcfec82ee0fe75f6a3d604cc3baae4aa6
吴可 <wuke6666@163.com>
2018-08-15 19:02:23 +0800

    继续上传Caliburn.Micro教程代码3

---
+++ 3EventAggregatorHandler/ViewModels/OneViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Caliburn.Micro;
+using Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3EventAggregatorHandler.ViewModels
+{
+    [Export(typeof(OneViewModel))]
+    public class OneViewModel : PropertyChangedBase, IHandle<string>
+    {
+        private readonly IEventAggregator _event;
+        private string _oneMessage;
+        public string OneMessage
+        {
+            get { return _oneMessage; }
+            set
+            {
+                _oneMessage = value;
+                NotifyOfPropertyChange(()=>OneMessage);
+            }
+        }
+        [ImportingConstructor]
+        public OneViewModel()
+        {
+            _event = IoC.Get<IEventAggregator>();
+            _event.Subscribe(this);
+        }
+        public void Handle(string message)
+        {
+            OneMessage = message;
+        }
+        public void Publish()
+        {
+            _event.PublishOnUIThread(new MyMessage { Str = $"{OneMessage} One!" });
+        }
+    }
+}
---
+++ 3EventAggregatorHandler/ViewModels/ShellViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Caliburn.Micro;
+using Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3EventAggregatorHandler.ViewModels
+{
+    [Export(typeof(IShell))]
+    public class ShellViewModel:PropertyChangedBase,IShell,IHandle<string>,IHandle<MyMessage>
+    {
+        private IEventAggregator _eventAggregator;
+        private IWindowManager _windowManager;
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                _message = value;
+                NotifyOfPropertyChange(()=>Message);
+            }
+        }
+        [ImportingConstructor]
+        public ShellViewModel(IWindowManager manager,IEventAggregator aggregator)
+        {
+            _eventAggregator = aggregator;
+            _windowManager = manager;
+            _eventAggregator.Subscribe(this);
+        }
+
+        public void Handle(string message)
+        {
+            Message = message;
+        }
+
+        public void Handle(MyMessage message)
+        {
+            Message = message.Str;
+        }
+
+        public void Publish()
+        {
+            _eventAggregator.PublishOnUIThread(Message);
+        }
+        public void OpenOneWin()
+        {
+            OneViewModel one = new OneViewModel();
+            _windowManager.ShowWindow(one);
+        }
+    }
+}
